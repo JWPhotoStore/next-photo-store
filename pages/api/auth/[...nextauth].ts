2157c3b95ac65c12866b290520954b0033839d7c
JayC1765 <Jason.chann91@gmail.com>
2023-04-05 16:24:21 -0400

    set up stripe api

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,6 +2,7 @@ import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import { PrismaClient } from '@prisma/client';
+import Stripe from 'stripe';
 
 const prisma = new PrismaClient();
 
@@ -14,4 +15,23 @@ export default NextAuth({
     }),
     // Add another provider
   ],
+  events: {
+    createUser: async ({ user }) => {
+      const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
+        apiVersion: '2022-11-15',
+      });
+
+      if (user.name && user.email) {
+        const customer = await stripe.customers.create({
+          email: user.email,
+          name: user.name,
+        });
+        // Also update our prisma user with the stripecustomerid
+        await prisma.user.update({
+          where: { id: user.id },
+          data: { stripeCustomerId: customer.id },
+        });
+      }
+    },
+  },
 });
--- prisma/migrations/20230405192236_we_removed_the_todo_model_and_added_the_user_model/migration.sql
+++ prisma/migrations/20230405192236_we_removed_the_todo_model_and_added_the_user_model/migration.sql
@@ -4,9 +4,6 @@
   - You are about to drop the `Todo` table. If the table is not empty, all the data it contains will be lost.
 
 */
--- DropTable
-DROP TABLE "Todo";
-
 -- CreateTable
 CREATE TABLE "Account" (
     "id" TEXT NOT NULL,
