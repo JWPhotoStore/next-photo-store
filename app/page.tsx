ecd440f65c7c06f1460298d0c7a1a0b1542e95f9
William Ku <william.ku9192@gmail.com>
2023-04-06 22:33:30 -0400

    fetch stripe products, render product component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,37 @@
-export default function Home() {
+import Stripe from "stripe";
+import Product from "./components/Product";
+
+const getProducts = async () => {
+  if (!process.env.STRIPE_SECRET_KEY)
+    throw new Error("Missing Stripe secret key");
+  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+    apiVersion: "2022-11-15",
+  });
+  const products = await stripe.products.list();
+  const productsWithPrices = await Promise.all(
+    products.data.map(async (product) => {
+      const prices = await stripe.prices.list({ product: product.id });
+      return {
+        id: product.id,
+        name: product.name,
+        description: product.description,
+        image: product.images[0],
+        currency: prices.data[0].currency,
+        price: prices.data[0].unit_amount,
+      };
+    })
+  );
+  return productsWithPrices;
+};
+
+export default async function Home() {
+  const products = await getProducts();
+
   return (
     <main>
-      <h1>Next App 13</h1>
+      {products.map((product) => (
+        <Product key={product.id} {...product} />
+      ))}
     </main>
   );
 }
