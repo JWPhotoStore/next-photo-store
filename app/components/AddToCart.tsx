0f214c3a7431f030bdcd9f40d5c748ca39cda98c
William Ku <william.ku9192@gmail.com>
2023-04-11 11:31:34 -0400

    rm comments

--- app/components/AddToCart.tsx
+++ app/components/AddToCart.tsx
@@ -2,16 +2,14 @@
 
 import styles from "@/styles/Product.module.css";
 import { SearchParamTypes } from "@/types/SearchParamsTypes";
-// import formatPrice from "@/util/PriceFormat";
 import { useSelector, useDispatch } from "react-redux";
 import { RootState } from "@/app/store/store";
 import { incrementQuantity, addCartItem } from "@/app/store/cartSlice";
 
 function AddToCart({ searchParams }: SearchParamTypes) {
-  // gets state from redux store
   const { cartItems } = useSelector((state: RootState) => state.cartReducer);
-  // actual dispatch function is dispatch = useDispatch()
   const dispatch = useDispatch();
+
   const handleAddToCart = () => {
     let cartItemToAdd = null;
     let isProductInCart = false;
