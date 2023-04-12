c59d046170a246ce6da127b7bc310388f26d5428
JayC1765 <Jason.chann91@gmail.com>
2023-04-12 15:29:17 -0400

    generated NextAuth secret and url

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -8,6 +8,7 @@ const prisma = new PrismaClient();
 
 export const authOptions = {
   adapter: PrismaAdapter(prisma),
+  secret: process.env.NEXTAUTH_SECRET,
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
