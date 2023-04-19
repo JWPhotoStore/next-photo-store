0d0a18a1a8bd0592217cdcfc956b327190632f33
JayC1765 <Jason.chann91@gmail.com>
2023-04-19 17:48:54 -0400

    moved code from api/orders to dashboard component

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,26 @@
-export default function Dashboard() {
+import { PrismaClient } from "@prisma/client";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
+
+const fetchOrders = async () => {
+  const prisma = new PrismaClient();
+  const userSession = await getServerSession(authOptions);
+  if (!userSession) {
+    return { message: "Not logged in" };
+  }
+
+  const orders = await prisma.order.findMany({
+    where: { userId: userSession?.user?.id },
+    include: {
+      products: true,
+    },
+  });
+
+  return orders;
+};
+
+export default async function Dashboard() {
+  const orders = await fetchOrders();
+  console.log(orders);
   return <div>Dashboard</div>;
 }
