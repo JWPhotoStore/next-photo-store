7578ce9361a8c12c5f986b74b158813a740692e1
William Ku <william.ku9192@gmail.com>
2023-04-29 23:23:53 -0400

    changes ui reducer functions

--- app/store/uiSlice.ts
+++ app/store/uiSlice.ts
@@ -1,5 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
 import type { PayloadAction } from "@reduxjs/toolkit";
+import { CartItemTypes } from "@/types/CartItemTypes";
 
 export interface UIState {
   isMobileMenuOpen: boolean;
@@ -13,13 +14,16 @@ export const uiSlice = createSlice({
   name: "ui",
   initialState,
   reducers: {
-    toggleMobileMenu: (state) => {
-      state.isMobileMenuOpen = !state.isMobileMenuOpen;
+    openMobileMenu: (state) => {
+      state.isMobileMenuOpen = true;
+    },
+    closeMobileMenu: (state) => {
+      state.isMobileMenuOpen = false;
     },
   },
 });
 
 // Action creators are generated for each case reducer function
-export const { toggleMobileMenu } = uiSlice.actions;
+export const { openMobileMenu, closeMobileMenu } = uiSlice.actions;
 
 export default uiSlice.reducer;
