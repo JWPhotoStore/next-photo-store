07886c8d55563aa677f5de2eb1d844ab8f9c2e40
William Ku <william.ku9192@gmail.com>
2023-05-09 19:51:40 -0400

    implements emailjs

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,27 +1,72 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import styles from "@/styles/ContactForm.module.css";
+import emailjs from "@emailjs/browser";
 
 export default function EmailForm() {
+  const form = useRef<HTMLFormElement>(null);
   const [name, setName] = useState("");
   const [phoneNumber, setPhoneNumber] = useState("");
   const [email, setEmail] = useState("");
   const [title, setTitle] = useState("");
   const [message, setMessage] = useState("");
+  const [formSending, setFormSending] = useState(false);
+  const [isVarMissing, setIsVarMissing] = useState(true);
 
-  function handleSubmit(event: React.SyntheticEvent) {
-    console.log("Form is not functional yet.");
+  const clearForm = () => {
+    setName("");
+    setPhoneNumber("");
+    setEmail("");
+    setTitle("");
+    setMessage("");
+  };
+
+  useEffect(() => {
+    if (
+      process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY &&
+      process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID &&
+      process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID
+    )
+      setIsVarMissing(false);
+  }, []);
+
+  function handleSubmit(e: React.SyntheticEvent) {
+    e.preventDefault();
+    setFormSending(true);
+    emailjs
+      .sendForm(
+        process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID as string,
+        process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID as string,
+        form.current,
+        process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY as string
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          clearForm();
+          setFormSending(false);
+        },
+        (error) => {
+          console.log(error.text);
+          setFormSending(false);
+        }
+      );
     //TODO: add email string validation and then show warning message next to input
   }
 
   return (
-    <form onSubmit={handleSubmit} className={styles.contactFormContainer}>
+    <form
+      ref={form}
+      onSubmit={handleSubmit}
+      className={styles.contactFormContainer}
+    >
       <div className={styles.formGroup}>
         <label htmlFor="name">Name</label>
         <input
           type="text"
           id="name"
+          name="name"
           value={name}
           onChange={(event) => setName(event.target.value)}
         />
@@ -31,6 +76,7 @@ export default function EmailForm() {
         <input
           type="tel"
           id="phoneNumber"
+          name="phone_number"
           value={phoneNumber}
           onChange={(event) => setPhoneNumber(event.target.value)}
         />
@@ -40,6 +86,7 @@ export default function EmailForm() {
         <input
           type="email"
           id="email"
+          name="email"
           value={email}
           onChange={(event) => setEmail(event.target.value)}
         />
@@ -49,6 +96,7 @@ export default function EmailForm() {
         <input
           type="text"
           id="title"
+          name="title"
           value={title}
           onChange={(event) => setTitle(event.target.value)}
         />
@@ -57,12 +105,17 @@ export default function EmailForm() {
         <label htmlFor="message">Message</label>
         <textarea
           id="message"
+          name="message"
           value={message}
           className={styles.messageTextArea}
           onChange={(event) => setMessage(event.target.value)}
         />
       </div>
-      <button className="primaryButton" type="submit">
+      <button
+        className="primaryButton"
+        type="submit"
+        disabled={isVarMissing || formSending}
+      >
         Send Email
       </button>
     </form>
