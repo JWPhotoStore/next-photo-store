29f6c5a309042b458773177121b487d16843e10f
William Ku <william.ku9192@gmail.com>
2023-05-11 15:52:38 -0400

    added 200 to add-to-cart endpoint

--- pages/api/add-to-cart.ts
+++ pages/api/add-to-cart.ts
@@ -98,6 +98,7 @@ export default async function handler(
           },
         },
       });
+      res.status(200).send({ message: "Existing cart item updated in order." });
     }
     //case when adding a new cartItem to the user's cart
     else {
@@ -124,7 +125,7 @@ export default async function handler(
         },
       });
     }
-
+    res.status(200).send({ message: "New cart item added to order." });
     //TODO: Return all the cartItems back to the client
   } else {
     console.log("there is NO paymentIntentID yet");
