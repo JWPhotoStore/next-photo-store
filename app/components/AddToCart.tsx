9d060568ed3b02e11d12d7ddce920171c2c927b0
JayC1765 <Jason.chann91@gmail.com>
2023-05-11 17:02:49 -0400

    disabled add-to-cart button when loading

--- app/components/AddToCart.tsx
+++ app/components/AddToCart.tsx
@@ -3,8 +3,6 @@
 import { ProductType } from "@/types/ProductType";
 import { useSelector, useDispatch } from "react-redux";
 import { RootState } from "@/app/store/store";
-import { setPaymentIntent } from "../store/stripeSlice";
-// import { addCartItem } from "../store/cartSlice";
 import { useAddCartItemMutation } from "../store/apiSlice";
 import React from "react";
 
@@ -24,43 +22,24 @@ export default function AddToCart({
   const { paymentIntentID } = useSelector(
     (state: RootState) => state.stripeReducer
   );
-  // const { cartItems } = useSelector((state: RootState) => state.cartReducer);
-  // const dispatch = useDispatch();
 
-  const [addCartItem] = useAddCartItemMutation();
-
-  const handleAdd = async (e: React.SyntheticEvent) => {
-    //TODO: Look into how to get back the response object - Currently work working
-    addCartItem({
-      name,
-      description,
-      image,
-      unit_amount,
-      currency,
-      quantity,
-      paymentIntentID,
-      stripeProductId: id,
-    })
-      .unwrap()
-      .then((data) => console.log(data));
-  };
+  const [addCartItem, { isLoading }] = useAddCartItemMutation();
 
   return (
     <button
-      onClick={
-        // () =>
-        //   addCartItem({
-        //     name,
-        //     description,
-        //     image,
-        //     unit_amount,
-        //     currency,
-        //     quantity,
-        //     paymentIntentID,
-        //     stripeProductId: id,
-        //   })
-        (e) => handleAdd(e)
+      onClick={() =>
+        addCartItem({
+          name,
+          description,
+          image,
+          unit_amount,
+          currency,
+          quantity,
+          paymentIntentID,
+          stripeProductId: id,
+        })
       }
+      disabled={isLoading}
     >
       Add To Cart
     </button>
